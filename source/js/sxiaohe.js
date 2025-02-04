45e6a20272a9efd74715e0ad80e89f46c38af2c1
661111 <xiaoxin104@outlook.com>
2025-02-04 10:31:14 +0800

    添加hometop源文件（留意节点）

--- source/js/sxiaohe.js
+++ source/js/sxiaohe.js
@@ -1,3 +1,4 @@
+/* switchDarkMode JS */
 function switchDarkMode() { // Switch Between Light And Dark Mode
     const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
     if (nowMode === 'light') {
@@ -14,3 +15,15 @@ function switchDarkMode() { // Switch Between Light And Dark Mode
     typeof FB === 'object' && window.loadFBComment()
     window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200)
   }
+/* hometop JS */
+var bywind = {
+    hideTodayCard: function() {
+        document.getElementById("todayCard") && document.getElementById("todayCard").classList.add("hide")
+    }
+}
+$(".topGroup").hover((function() {}
+), (function() {
+        document.getElementById("todayCard").classList.remove("hide"),
+        document.getElementById("todayCard").style.zIndex = 1
+    }
+))
